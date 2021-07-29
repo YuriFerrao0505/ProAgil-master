75f8dfac293271f0d57a5104ec17086bf20a06e9
Yuri Ferrão <yuriferrao9@gmail.com>
2021-07-29 15:51:53 -0300

    Angular - Organizando Projeto

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -2,25 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-
+import {TooltipModule} from "ngx-bootstrap/tooltip";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {ModalModule} from "ngx-bootstrap/modal";
 import { AppRoutingModule } from './app-routing.module';
+import { AccordionModule } from "ngx-bootstrap/accordion";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { EventoService } from './_services/evento.service';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 
 @NgModule({
   declarations: [		
     AppComponent,
       EventosComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipePipe
    ],
   imports: [
     BrowserModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventoService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
